b13dbfb6d6c4799f0b416aaf0032a4deab2a79b0
NguyenTKhoi <90817289+NguyenTKhoi@users.noreply.github.com>
2022-08-17 12:40:54 +0700

    Add files via upload

---
+++ database/db.php
@@ -0,0 +1,5 @@
+<?php 
+
+$db = pg_connect("host=ec2-44-205-112-253.compute-1.amazonaws.com dbname=d2nlbeglsecip0 user=ydypewlqkahcdd password=6a6cdfac81a598105e12c69d03feed05cd392af4c48efbd082dd52928e8d3594 port=5432");
+
+?>
